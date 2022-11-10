8f0fde480ccf589262723bedb175068f2c8cf18d
einnarth <113421210+einnarth@users.noreply.github.com>
2022-11-10 08:32:49 +0100

    Update StringTools.cs

--- Open-Lab-04.13/StringTools.cs
+++ Open-Lab-04.13/StringTools.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_04._13
 {
@@ -6,7 +6,29 @@ namespace Open_Lab_04._13
     {
         public string GetLongestCommonSequence(string str1, string str2)
         {
-            throw new NotImplementedException();
+            var lengths = new int[str1.Length, str2.Length];
+            int greatestLength = 0;
+            string output = "";
+            for (int i = 0; i < str1.Length; i++)
+            {
+                for (int j = 0; j < str2.Length; j++)
+                {
+                    if (str1[i] == str2[j])
+                    {
+                        lengths[i, j] = i == 0 || j == 0 ? 1 : lengths[i - 1, j - 1] + 1;
+                        if (lengths[i, j] > greatestLength)
+                        {
+                            greatestLength = lengths[i, j];
+                            output = str1.Substring(i - greatestLength + 1, greatestLength);
+                        }
+                    }
+                    else
+                    {
+                        lengths[i, j] = 0;
+                    }
+                }
+            }
+            return output;
+        }
         }
     }
-}
